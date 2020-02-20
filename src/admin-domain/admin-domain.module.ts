65edd28557139d24e2d4ca22281fa1a5ae3e9f7b
Francisco F. Cardoso <francardoso@outlook.com>
2020-02-19 21:45:07 -0300

    tirando coisa que nao precisa

--- src/admin-domain/admin-domain.module.ts
+++ src/admin-domain/admin-domain.module.ts
@@ -10,13 +10,11 @@ import { Massotherapist } from './massotherapists/massotherapist.entity';
 import { Schedule } from './schedule/schedule.entity';
 import { CompaniesService } from './companies/companies.service';
 import { ScheduleService } from './schedule/schedule.service';
-import { AvailableDaysController } from './available-days/available-days.controller';
-import { AvailableDaysService } from './available-days/available-days.service';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Client, Company, Massotherapist, Schedule])],
-  controllers: [CompaniesController, ScheduleController, MassotherapistsController, ClientsController, AvailableDaysController],
+  imports: [TypeOrmModule.forFeature([Client, Company, Massotherapist, Schedule])],
+  controllers: [CompaniesController, ScheduleController, MassotherapistsController, ClientsController],
   exports: [TypeOrmModule],
-  providers: [CompaniesService, ScheduleService, AvailableDaysService],
+  providers: [CompaniesService, ScheduleService],
 })
 export class AdminDomainModule {}
